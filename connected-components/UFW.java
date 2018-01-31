683a5a8e8dfe41c1cf8413bc517c65dcc3b7cd3d
Vilfred <Vilfredsikker@gmail.com>
2018-01-31 14:35:37 +0100

    Update UFW with a new STVM. Tests true with TinyUF.txt

--- connected-components/UFW.java
+++ connected-components/UFW.java
@@ -1,7 +1,17 @@
 import edu.princeton.cs.algs4.*;
 
-public class UFW {
-    public static void main (String[] args) {
-        StdOut.println("Hello world!");
+public class UFW{
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        UF uf = new UF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.connected(1,0));
     }
+
 }
