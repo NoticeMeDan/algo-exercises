0bb85aa43093be753efd048c25879532d1a958a5
Beta <im.arnild@gmail.com>
2018-02-08 15:56:39 +0100

    ...

---
+++ GiantBook/Magnus/GiantBook.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.algs4.*;
+
+public class GiantBook{
+
+	private HashSet<Integer> = new HashSet<Integer>();
+	private int T;
+	private int N;
+
+	public static void main(String[] args){
+		if (args.lenght>0){
+			T = Double.parseDouble(args[0]);
+			N = Double.parsDouble(args[1]);
+		}
+	}
+	
+}
