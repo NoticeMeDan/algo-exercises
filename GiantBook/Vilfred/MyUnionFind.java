a965c089c49ed795f10dbf1a67975d5c3b23458a
Vilfred <Vilfredsikker@gmail.com>
2018-02-11 15:40:11 +0100

    MyUnionFind updated

--- GiantBook/Vilfred/MyUnionFind.java
+++ GiantBook/Vilfred/MyUnionFind.java
@@ -1,12 +1,19 @@
 import edu.princeton.cs.algs4.*;
+import java.util.*;
 
+/**
+ * A modified version of WeightedQuickUnionFind
+ * (M) will occur at places that have been modified
+ */
 public class MyUnionFind {
     private int[] parent;   // parent[i] = parent of i
     private int[] size;     // size[i] = number of sites in subtree rooted at i
     private int count;      // number of components
+    private int biggestComponentSize; // (M) the size of the current biggest component
+    private Set<Integer> isolatedIndividuals; // (M) set containing sites that are isolated
 
     /**
-     * Initializes an empty union–find data structure with {@code n} sites
+     * Initializes an empty unionfind data structure with {@code n} sites
      * {@code 0} through {@code n-1}. Each site is initially in its own 
      * component.
      *
@@ -14,13 +21,30 @@ public class MyUnionFind {
      * @throws IllegalArgumentException if {@code n < 0}
      */
     public MyUnionFind(int n) {
+        biggestComponentSize = 1; // (M)
+        isolatedIndividuals = new HashSet<Integer>(); // (M)
         count = n;
         parent = new int[n];
         size = new int[n];
         for (int i = 0; i < n; i++) {
+            isolatedIndividuals.add(i); // (M)
             parent[i] = i;
             size[i] = 1;
         }
+    }
+    
+    /**
+     * (M) Return biggestComponentSize
+     */
+    public int getBiggestComponentSize() {
+        return biggestComponentSize;
+    }
+    
+    /**
+     * (M) Check if there exists isolated individuals
+     */
+    public boolean noIndividualsIsIsolated() {
+        return isolatedIndividuals.isEmpty();
     }
 
     /**
@@ -86,22 +110,21 @@ public class MyUnionFind {
         if (size[rootP] < size[rootQ]) {
             parent[rootP] = rootQ;
             size[rootQ] += size[rootP];
+            
+            // (M) Update biggestComponentSize
+            if (size[rootQ] > biggestComponentSize) biggestComponentSize = size[rootQ];
         }
         else {
             parent[rootQ] = rootP;
             size[rootP] += size[rootQ];
+            
+            // (M) Update biggestComponentSize
+            if (size[rootP] > biggestComponentSize) biggestComponentSize = size[rootQ];
         }
+        
+        isolatedIndividuals.remove(rootP);
+        isolatedIndividuals.remove(rootQ);
+        
         count--;
     }
-
-
-    /**
-     * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input, 
-     * where each integer represents some object;
-     * if the sites are in different components, merge the two components
-     * and print the pair to standard output.
-     *
-     * @param args the command-line arguments
-     */
-    
 }
