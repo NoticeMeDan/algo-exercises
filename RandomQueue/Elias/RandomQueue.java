c8f70bc9c2cdafcb167632e67f70169ad143c9fb
Elias Jørgensen <eliaszakarias@gmail.com>
2018-02-15 13:38:04 +0100

    RandomQueue formatting

--- RandomQueue/Elias/RandomQueue.java
+++ RandomQueue/Elias/RandomQueue.java
@@ -80,7 +80,6 @@ public class RandomQueue<T> implements Iterable<T> {
 		int index;
 		T[] q;
 
-		@SuppressWarnings("unchecked")
 		public RandomQueueIterator(T[] items) {
 			this.index = 0;
 			this.q = items;
@@ -94,6 +93,7 @@ public class RandomQueue<T> implements Iterable<T> {
 
 		public T next() {
 			if (!hasNext()) throw new NoSuchElementException();
+
 			T item = q[index];
 			this.index++;
 			return item;
