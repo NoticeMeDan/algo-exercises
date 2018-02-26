dae2c16de495d565cd4dad5c05cb1be72c62e58a
Vilfred <Vilfredsikker@gmail.com>
2018-02-26 11:33:36 +0100

    Changed to complete Code Judge

--- Foursum/Vilfred/src/scripts/Simple.sh
+++ Foursum/Vilfred/src/scripts/Simple.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 cd ..
 
+javac-algs4 Simple.java
 
 for filename in ../data/data_small/*.txt; do
 	echo "File: " [$(basename "${filename}")]
 	start=`date +%s`
-	python Simple.py < "$filename"
+	java-algs4 Simple.java < "$filename"
 	end=`date +%s`
 
 	printf "Seconds for completion: $((end - start)) \n \n"
