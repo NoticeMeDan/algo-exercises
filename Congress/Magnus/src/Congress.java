c4e9345ebe5a4ba904710ede54ac3680dfbcd6ee
IMarnild <im.arnild@gmail.com>
2018-03-03 14:44:35 +0100

    Congress

--- Congress/Magnus/src/Congress.java
+++ Congress/Magnus/src/Congress.java
@@ -1,36 +1,56 @@
 import edu.princeton.cs.algs4.*;
 
-public class Congress{
+public class Congress {
 	
-	private int seats;
 	private int states;
+	private int seats;
 	private MaxPQ<State> pq;
 
 	public Congress(int states, int seats){
 		this.states = states;
 		this.seats = seats;
-		this.pq = new MaxPQ<State>(states);
+		this.pq = new MaxPQ<State>();
 	}
 
-	public void addState(State state){
-		this.pq.insert(state);
+	private void insertState(State state){
+		pq.insert(state);
 	}
 
-	public int getSeats(){
+	private State delegateSeat(State state){
+		state.addSeat();
+		this.seats--;
+		return state;
+	}
+
+	private int getSeats(){
 		return this.seats;
 	}
 
-	public int getStates(){
-		return this.states;
-	}
-
-	public State delMax(){
+	private State delMax(){
 		return this.pq.delMax();
 	}
 
-	public void print(){
+	private void print(){
 		for(State s : this.pq){
 			System.out.println(s);
-		}	
+		}
+	}
+
+	public static void main(String[]args){
+		Congress c = new Congress(StdIn.readInt(),StdIn.readInt());
+
+		while(!StdIn.isEmpty()){
+			StdIn.readLine();
+			State state = new State(StdIn.readLine(),StdIn.readDouble());
+			c.delegateSeat(state);
+			c.insertState(state);
+		}
+
+		while(c.getSeats()!=0){
+			State state = c.delMax();
+			state = c.delegateSeat(state);
+			c.insertState(state);
+		}
+		c.print();
 	}
 }
