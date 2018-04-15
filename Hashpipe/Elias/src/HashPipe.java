84e079e372d9567205468885824b9e46525b7898
Elias Jørgensen <eliaszakarias@gmail.com>
2018-04-15 21:04:00 +0200

    Things are working

--- Hashpipe/Elias/src/HashPipe.java
+++ Hashpipe/Elias/src/HashPipe.java
@@ -1,17 +1,20 @@
-import edu.princeton.cs.algs4.*;
 import java.util.Optional;
 
 public class HashPipe {
-	private int size;
 	private Pipe rootPipe;
+	private Pipe floorPipe;
+	private int size;
 
 	public HashPipe() {
-		this.size = 0;
 		this.rootPipe = new Pipe();
+		//this.floorPipe = new Pipe();
+		this.size = 0;
 	}
 
 	// Return number of elements
-	public int size() { return this.size; }
+	public int size() {
+		return size;
+	}
 
 	// Put key/val pair into the table
 	public void put(String key, Integer val) {
@@ -22,19 +25,25 @@ public class HashPipe {
 
 	// Value associated with key
 	public Integer get(String key) {
-		Optional<Pipe> pipe = this.getPipe(this.rootPipe, key, this.rootPipe.getHeight());
+		Optional<Pipe> pipe = getPipe(this.rootPipe, key, this.rootPipe.getHeight());
 		return pipe.isPresent() ? pipe.get().getValue() : null;
 	}
 
-	// Largest key less than or equal to key
-	public String floor(String key) { return null; }
+//    // Largest key less than or equal to key
+//    public String floor(String key) {
+//        if (!floorPipe.getKey().equals(key)) {
+//            Pipe pipe = getPipe(rootPipe, key, rootPipe.getHeight() -1);
+//            return (pipe != null && pipe.getKey().equals(key)) ? pipe.getKey() : floorPipe.getKey();
+//        }
+//        return floorPipe.getKey();
+//    }
 
 	// Return content of pipe at given key and height
 	public String control(String key, int h) {
-		if (this.getPipeHeight(key) < h) return null;
-		Optional<Pipe> pipe = this.getPipe(this.rootPipe, key, h -1 );
+		if (getPipeHeight(key) < (h + 1)) return null;
+		Optional<Pipe> pipe = this.getPipe(this.rootPipe, key, h);
 		if (pipe.isPresent()) {
-			Optional<Pipe> nextPipe = pipe.get().getNextPipe(h - 1);
+			Optional<Pipe> nextPipe = pipe.get().getNextPipe(h);
 			return nextPipe.map(Pipe::getKey).orElse(null);
 		}
 		return null;
@@ -47,7 +56,7 @@ public class HashPipe {
 
 		if (!nextPipe.isPresent()) {
 			from.setNextPipe(to, height);
-			this.insertPipe(from, to, height - 1);
+			insertPipe(from, to, height - 1);
 		} else {
 			if (nextPipe.get().getKey().compareTo(to.getKey()) < 0) {
 				this.insertPipe(nextPipe.get(), to, height);
@@ -65,17 +74,16 @@ public class HashPipe {
 		if (height < 0) return Optional.empty();
 		Optional<Pipe> nextPipe = from.getNextPipe(height);
 
-		if (!nextPipe.isPresent()) { return this.getPipe(from, key, height - 1); }
+		if (!nextPipe.isPresent()) { return getPipe(from, key, height - 1); }
 		else {
 			if (nextPipe.get().getKey().compareTo(key) < 0) {
-				this.getPipe(nextPipe.get(), key, height);
+				return getPipe(nextPipe.get(), key, height);
 			}
 			else if (nextPipe.get().getKey().compareTo(key) > 0) {
-				this.getPipe(from, key, height - 1);
+				return getPipe(from, key, height - 1);
 			}
 			else return nextPipe;
 		}
-		return Optional.empty();
 	}
 
 	private int getPipeHeight(String key) {
@@ -104,9 +112,9 @@ public class HashPipe {
 			this.elements = new PipeProxy[getPipeHeight(key)];
 		}
 
-		public String getKey() { return this.key; }
+		String getKey() { return this.key; }
 
-		public int getValue() { return this.value; }
+		int getValue() { return this.value; }
 
 		Optional<Pipe> getNextPipe(int index) {
 			return this.elements[index] == null ? Optional.empty() : Optional.of(this.elements[index].pipe);
@@ -118,29 +126,29 @@ public class HashPipe {
 	}
 
 	public static void main(String[] args) {
-		 // Test
-         int i=0;
-         //        String [] in = new String[0];
-         String [] in = new String[26];
-         i=0;
-         for(char c = 'A'; c <= 'Z'; c++ ) in[i++] = "" + c;
+		// Test
+		int i=0;
+		//        String [] in = new String[0];
+		String [] in = new String[26];
+		i=0;
+		for(char c = 'A'; c <= 'Z'; c++ ) in[i++] = "" + c;
 
-         HashPipe H = new HashPipe();
+		HashPipe H = new HashPipe();
 
-         for( int j=0;j<in.length;j++ ) {
-             H.put(in[j], j);
-             System.out.print("Insert: ");
-             System.out.println(in[j]);
-             for( int g=0;g<j;g++ ) {
-                 for( int h=0;h<32;h++ ) {
-                     String ctrl = H.control(in[g],h);
-                     if( ctrl != null ) System.out.print(ctrl);
-                     else System.out.print(".");
-                     System.out.print(" ");
-                 }
-                 System.out.print(" : ");
-                 System.out.println(in[g]);
-             }
-         }
+		for( int j=0;j<in.length;j++ ) {
+			H.put(in[j], j);
+			System.out.print("Insert: ");
+			System.out.println(in[j]);
+			for( int g=0;g<j;g++ ) {
+				for( int h=0;h<32;h++ ) {
+					String ctrl = H.control(in[g],h);
+					if( ctrl != null ) System.out.print(ctrl);
+					else System.out.print(".");
+					System.out.print(" ");
+				}
+				System.out.print(" : ");
+				System.out.println(in[g]);
+			}
+		}
 	}
 }
