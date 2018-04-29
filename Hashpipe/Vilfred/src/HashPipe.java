c97c4a61b19c1e75dae5f6c815bf12bbd22e1e7d
Vilfred <Vilfredsikker@gmail.com>
2018-04-29 12:36:25 +0200

    New HashPipe

--- Hashpipe/Vilfred/src/HashPipe.java
+++ Hashpipe/Vilfred/src/HashPipe.java
@@ -1,107 +1,123 @@
-import edu.princeton.cs.algs4.*;
+public class HashPipe {
+    private Pipe root;
 
-public class HashPipe{
-  private RedBlackBST<String, Integer>[] trees;
-  private int size;
-
-  public HashPipe(){ // create an empty symbol table
-    this.trees = new RedBlackBST[32]; //Default for our HashPipe
-    this.size = 0; //Default size
-
-    for(int i = 0; i<32; i++){ //Create a RedBlackBST for each index.
-      this.trees[i] = new RedBlackBST<String, Integer>();
-    }
-  }
-
-  public int size(){ // return the number of elements
-    return this.size;
-  }
-
-  // The rule for put() is the following:
-  // The index of the array holds it own height and any height above itself
-  // Therefore trees[0] holds every key and value. Trees[4] holds height 4 and everything above.
-  public void put(String key, Integer val){ // put key-value pair into the table
-    int height = getKeyHeight(key);
-
-    for (int i = 0; i < height; i++){ // do put() as long as the rule holds
-      this.trees[i].put(key, val);
+    public HashPipe() {
+        // Default height set to 32
+        this.root = new Pipe(null, 0, 32);
     }
 
-    this.size ++; //Finally increase the size of the Pipe
-  }
-
-  public Integer get(String key){  // value associated with key
-    int height = getKeyHeight(key); //get the height
-
-    return this.trees[height].get(key);
-  }
-
-  public String floor(String key){ // largest key less than or equal to key
-    int height = getKeyHeight(key);
-
-    return this.trees[height].floor(key);
-  }
-
-  //Helper Functions:
-  private int getKeyHeight(String key){
-    int keyHashValue = key.hashCode(); //Get the HashValue for the character
-    int trailingZeros = Integer.numberOfTrailingZeros(keyHashValue); //Get the number of trailingZeros
-    int height = trailingZeros += 1; //the height is trailingZeros + 1
-
-    return height;
-  }
-
-  // returns the contents of the pipe of the given key at the
-  // given height h, counting from below and starting with 0.
-  public String control(String key, int h){
-    try{ //Try because the key might not exist in the given height
-      String controlKey = trees[h].ceiling(key); //Enters the height, finds the ceiling to the key
-
-      if (controlKey.equals(key)){
-        // key + 1, because ceiling
-        // Returns the smallest key in the symbol table greater than or equal to the key
-        return trees[h].ceiling(key + 1);
-      } else {
-        return null;
-      }
-    } catch (Exception e){
-      e.getMessage();
-      return null;
+    // put key-value pair into the pipe
+    public void put(String key, Integer val) {
+        // Create a new pipe
+        Pipe newPipe = new Pipe(key, val, Integer.numberOfTrailingZeros(key.hashCode()));
+        // Insert the new pipe into the HashPipe
+        put(this.root, newPipe, this.root.getHeight());
     }
-  }
 
-  //Main
-  public static void main(String[] args){
-    // // Test
-    // int i=0;
-    // //        String [] in = new String[0];
-    // String [] in = new String[26];
-    // i=0;
-    // for(char c = 'A'; c <= 'Z'; c++ ) in[i++] = "" + c;
-    //
-    // HashPipe H = new HashPipe();
-    //
-    // for( int j=0;j<in.length;j++ ){
-    //   H.put(in[j], j);
-    //   System.out.print("Insert: ");
-    //   System.out.println(in[j]);
-    //   for( int g=0;g<j;g++ ) {
-    //       for( int h=0;h<32;h++ ) {
-    //           String ctrl = H.control(in[g],h);
-    //           if( ctrl != null ) System.out.print(ctrl);
-    //           else System.out.print(".");
-    //           System.out.print(" ");
-    //       }
-    //       System.out.print(" : ");
-    //       System.out.println(in[g]);
-    //   }
-    // }
-    HashPipe hashPipe = new HashPipe();
-    int counter = 0;
+    private Pipe put(Pipe from, Pipe to, int height) {
+        // Checking if the pipe is valid
+        if (from == null || height < 0)
+            return null;
 
-    while(!StdIn.isEmpty()){
-      hashPipe.put(StdIn.readString(), counter);
-      counter++;
+        // Create a reference pipe
+        Pipe reference = from.pipes[height];
+        if (reference == null) {
+            // If the height is greater than our from, then our to, will be the referenced pipe
+            if (to.getHeight() >= height) {
+                setReference(from, to, height);
+            }
+            return put(from,to,--height);
+        }
+
+        int compare = to.getKey().compareTo(reference.getKey());
+        if (compare < 0) {
+            if (to.getHeight() >= height) setReference(from,to,height);
+            return put(from, to ,height-1);
+        } else if (compare > 0) {
+            return put(reference, to, height);
+        } else {
+            reference.setValue(to.getValue());
+            return reference;
+        }
     }
-  }
+
+    private void setReference(Pipe from, Pipe to, int height) {
+        Pipe reference = from.pipes[height];
+        from.pipes[height] = to;
+        to.pipes[height] = reference;
+    }
+
+    //returns the pipe of the floor of the given key
+    private Pipe floorPipe(Pipe x, String key, int height) {
+        if (x == null)
+            return null;
+
+        Pipe reference = x.pipes[height];
+        if (reference == null) {
+            if (height < 1)
+                return x;
+            else
+                return floorPipe(x, key, (height-1));
+        }
+
+        int compare = key.compareTo(reference.getKey());
+        if (compare < 0 && 0 < height)
+            return floorPipe(x , key , height-1);
+        else if (compare < 0)
+            return x;
+        if (compare == 0)
+            return reference;
+
+        Pipe floorPipe = floorPipe(reference, key, height);
+        if (floorPipe == null)
+            return reference;
+        else
+            return floorPipe;
+    }
+
+    /**
+     * Input is the pipe we want to control. The return values is the referenced pipe.
+     */
+    public String control(String key, int h) {
+        Pipe floorPipe= floorPipe(this.root, key, this.root.getHeight());
+        if (floorPipe == null || floorPipe.getHeight() < h || floorPipe.pipes[h] == null)
+            return null;
+
+        return floorPipe.pipes[h].getKey();
+    }
+
+    public class Pipe {
+        private String key;
+        private int value, height;
+        Pipe[] pipes;
+
+        Pipe(String key, int value, int height) {
+            this.key    = key;
+            this.value  = value;
+            this.height = height;
+            this.pipes  = new Pipe[height+1];
+        }
+
+        /*              *
+         *    GETTERS   *
+         *       &      *
+         *    SETTERS   *
+         *              */
+
+        String getKey() {
+            return this.key;
+        }
+
+        int getValue() {
+            return this.value;
+        }
+
+        int getHeight() {
+            return this.height;
+        }
+
+        void setValue(int value) {
+            this.value = value;
+        }
+    }
 }
